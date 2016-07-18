1c4825314946a8df69791a4a517de1deca388d77
hotta <g1327061@st.t-kougei.ac.jp>
2016-07-18 13:46:30 +0900

    簡単なレイアウト配置

---
+++ SmartPhone_Tiny_Breaker/Assets/Scripts/TapManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapManager : MonoBehaviour
+{
+    private GameObject buttonDownObj;     //仮の置物
+    private GameObject buttonUpObj;       //仮の置物
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 aTapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Collider2D aCollider2d = Physics2D.OverlapPoint(aTapPoint);
+
+        //タップ地点に当たり判定があった場合
+        if (aCollider2d)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //タップした地点のオブジェクト取得 
+                buttonDownObj = aCollider2d.gameObject;
+
+                //パワーアップボタンだった場合
+                if (buttonDownObj.tag == "PowerUp")
+                {
+                    //ボタンオブジェクトに入れてるButtonスクリプトのbuttonDownFlagをOnにして処理を実行させる。
+                    buttonDownObj.GetComponent<PowerUp>().buttonDownFlag = true;
+                }
+            }
+
+            if(Input.GetMouseButtonUp(0))
+            {
+                buttonUpObj = aCollider2d.gameObject;
+
+                //パワーアップボタンかつ同じオブジェクトか確認
+                if(buttonUpObj.tag == "PowerUp" && buttonUpObj == buttonDownObj)
+                {
+                    buttonUpObj.GetComponent<PowerUp>().runFlag = true;
+                }
+            }
+        }
+    }
+}
