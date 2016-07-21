8175ce5ee998b2c6e0040bb59ca8f7c34b4a7168
hotta <g1327061@st.t-kougei.ac.jp>
2016-07-21 22:41:20 +0900

    もう一回

---
+++ SmartPhone_Tiny_Breaker/Assets/Scripts/SpiritManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpiritManager : MonoBehaviour
+{
+    public GameObject spiritData;
+
+    public GameObject spiritHolder;
+
+    public int SpiritLimit = 5;
+
+    public static List<GameObject> spiritList = new List<GameObject>();
+
+    public static bool useSpiritFlag = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    public void useSpirit()
+    {
+        if (spiritList.Count > 0)
+        {
+            spiritList.RemoveAt(0);
+            useSpiritFlag = true;
+        }
+        else
+        {
+            useSpiritFlag = false;
+        }
+    }
+}
